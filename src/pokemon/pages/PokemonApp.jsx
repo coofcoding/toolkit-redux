89dda65269ba33ddb9629fee4733b0b52a525bc5
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-03 21:08:24 -0500

    show pokemon's names

--- src/pokemon/pages/PokemonApp.jsx
+++ src/pokemon/pages/PokemonApp.jsx
@@ -1,27 +1,41 @@
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { getPokemons } from "../../store/slices/pokemon/thunks";
+import { startLoadingPokemons, setPokemons } from '../../store';
+import { useSelector } from "react-redux";
 
 export const PokemonApp = () => {
 
+  const { pokemons, isLoading, page } = useSelector((state) => state.pokemons);
   const dispatch = useDispatch();
+
+  console.log(pokemons)
 
   useEffect(() => {
 
     dispatch(getPokemons());
 
-  }, [])
+  }, [page])
 
   return (
     <>
       <h1>PokemonApp</h1>
       <hr />
+      {
+        (isLoading) ? <span className="loading-alert">Loading...</span> : ''
+      }
 
-      <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
-      </ul>
+      {
+        pokemons.map((pokemon) => (
+          <div>
+            {pokemon.name}
+          </div>
+        ))
+      }
+
+      {/* <button onClick={ () => dispatch( getPokemons(page) ) }>
+        Next
+      </button> */}
     </>
   )
 }
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -5,33 +5,22 @@ export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
 
-        // TODO: realizar peticion http
-        // const resp = await fetch(`${import.meta.env.VITE_API_URL}pokemon?limit=10&offset=${page * 10}`);
-        // const data = await resp.json();
-        // console.log(data)
-
         const { data } = await pokemonApi.get(`pokemon?limit=10&offset=${page * 10}`);
 
         const withImagesPromises = data.results.map(async (pokemonInfo) => {
 
-            try {
-                const { data } = await pokemonApi.get(`pokemon/${pokemonInfo.name}`);
+            const { data } = await pokemonApi.get(`pokemon/${pokemonInfo.name}`);
 
-                const { front_default } = await data.sprites.other.showdown;
+            const { front_default } = await data.sprites.other.showdown;
 
-                return {
-                    ...pokemonInfo,
-                    gif: front_default
-                }
-
-            } catch (e) {
-                throw new Error('No data geted')
+            return {
+                ...pokemonInfo,
+                gif: front_default
             }
 
         })
 
         const withImages = await Promise.all(withImagesPromises);
-        console.log(withImages)
 
         dispatch(setPokemons({
             pokemons: withImages,
